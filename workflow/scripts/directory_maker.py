b415d70ae47c828923d237f888f225ce5e0d98aa
MaDLuck5 <matsslik5@gmail.com>
2024-06-23 12:00:53 +0200

    added create_directories logica method to the script

--- workflow/scripts/directory_maker.py
+++ workflow/scripts/directory_maker.py
@@ -4,6 +4,28 @@ import sys
 import yaml
 
 
+def create_directories(configuration_path):
+    # Load the configuration file
+    with open(configuration_path, 'r') as f:
+        config = yaml.safe_load(f)
+
+    # Define the list of directories from the config
+    directories = [
+        config["paths"]["raw_reads"],
+        config["paths"]["trimmed_reads"],
+        config["paths"]["aligned_reads"],
+        config["paths"]["assembled_contigs"],
+        config["paths"]["analyzed_metagenome"],
+        config["paths"]["visualizations"],
+        config["paths"]["coverage"],
+        config["paths"]["logs"],
+    ]
+
+    # Create directories if they don't exist
+    for directory in directories:
+        os.makedirs(directory, exist_ok=True)
+
+
 if __name__ == "__main__":
     # Expect the config file path as the first argument
     config_path = sys.argv[1]
